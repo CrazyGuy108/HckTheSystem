9846a02f25a03505598069a8946f452645e225fe
CrazyGuy108 <taylor@hansenfamily.us>
2018-09-22 15:51:32 -0700

    Add json task parser

---
+++ server/controllers/task/TaskBody.ts
@@ -0,0 +1,6 @@
+/** Typings for the task data post request. */
+export interface TaskBody
+{
+    task: string;
+    object: string;
+}
--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,11 @@
 import express from "express";
-import { controller } from "./controllers/controller";
+import { taskController } from "./controllers/task/controller";
 
 const app: express.Application = express();
 
 const port: number = +(process.env.PORT || 3000);
 
-app.use("/default", controller);
+app.use("/task", taskController);
 app.listen(port, () =>
 {
     console.log(`Listening at http://localhost:${port}/`);
