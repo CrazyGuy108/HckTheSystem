267d067877dcaa8254511ff7bf536e3823881b1e
orebitt <35861377+orebitt@users.noreply.github.com>
2018-09-22 20:42:03 -0700

    asfd
    afds

--- sensor/main.js
+++ sensor/main.js
@@ -5,10 +5,11 @@ var LoudnessAnalogSensor = GrovePi.sensors.LoudnessAnalog
 var board = new Board({
   debug: true,
   onError: function(err) {
-    console.log(err);
+    console.log('Error', err);
   },
   onInit: function(res) {
     if (res) {
+      console.log("REEAADDYY")
       var loudness = new LoudnessAnalogSensor(0);
       loudness.on('change', function(res) {
         console.log(res);
