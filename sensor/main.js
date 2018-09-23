7a185a36cd650e6e858a4c51395a5b9b0a294923
orebitt <35861377+orebitt@users.noreply.github.com>
2018-09-22 20:40:48 -0700

    fdsa
    fads

--- sensor/main.js
+++ sensor/main.js
@@ -1,5 +1,6 @@
 var GrovePi = require('node-grovepi').GrovePi;
 var Board = GrovePi.board;
+var LoudnessAnalogSensor = GrovePi.sensors.LoudnessAnalog
 
 var board = new Board({
   debug: true,
@@ -7,6 +8,13 @@ var board = new Board({
     console.log(err);
   },
   onInit: function(res) {
-    console.log(res);
+    if (res) {
+      var loudness = new LoudnessAnalogSensor(0);
+      loudness.on('change', function(res) {
+        console.log(res);
+      })
+      loudness.watch();
+    }
   }
 });
+board.init();
